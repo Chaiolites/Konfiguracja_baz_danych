5a589654f34f86b33ebf10f3cf54519d3fbfe528
Chaiolites <Chaiolites@gmail.com>
2025-07-04 10:16:19 +0200

    Dodanie pliku conf.py

---
+++ conf.py
@@ -0,0 +1,14 @@
+project = 'Konfiguracja baz danych'
+copyright = '2025,Piotr Domagała,Piotr Kotuła, Dawid Pasikowski'
+author =  'Piotr Domagała, Piotr Kotuła, Dawid Pasikowski'
+release = '1.0.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pl'
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
